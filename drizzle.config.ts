e63aca180f7e13ceaad966925a1dbbfb616dd504
Ibrahim <ali.ibrahim.3@lit-co.net>
2026-03-21 14:39:22 +0300

    create an index for all schemas so each feature has a separate schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "drizzle-kit";
 import env from "~/env";
 
 export default defineConfig({
-  schema: "./server/db/schema.ts",
+  schema: "./server/db/schema",
   out: "./server/db/migrations",
   dialect: "turso",
   casing: "snake_case",
